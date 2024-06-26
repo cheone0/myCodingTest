48088b8ed92ef35217a8117cd95916c8783442ae
HYE_YOUNG CHUN <103397192+cheone0@users.noreply.github.com>
2024-06-26 10:06:12 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+            	char[] answer1 = my_string.toCharArray();
+    	
+    	String answer = new String(answer1,0,n);    	
+   
+
+    	return answer;
+    }
+}
